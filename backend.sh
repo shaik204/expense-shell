718865e69fd2f078ba56ccd050454e4c73d9bf3a
Nihal <reenasheikh@gmail.com>
2024-10-12 12:21:41 +0530

    adding backend service correctly

--- backend.sh
+++ backend.sh
@@ -67,3 +67,7 @@ VALIDATE $? "removing old application files"
 
 unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting backend application code" 
+
+npm install &>>$LOG_FILE 
+echo -e "$G Current working directory: $(pwd) $N" | tee -a $LOG_FILE
+#cp /home/ec2-user/expense-shell/backend.service /etc/systemd/system/backend.service
